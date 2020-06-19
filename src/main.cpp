12e13d3f12f05d41c3fa0d6d13dd9c7b85189930
IlleQuiProgrammat <32514637+IlleQuiProgrammat@users.noreply.github.com>
2020-06-19 12:04:26 +0100

    Pixel Buffer Sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cstdint>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 void errorCallback(int error, const char* description) {
     std::cerr << "Error: " << description << " - code: " << error << std::endl;
@@ -15,16 +19,27 @@ int main() {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Pixels", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pixels", NULL, NULL);
     if (!window)
     {
         std::cerr << "GLFW window could not initialise" << std::endl;
         glfwTerminate();
         return -1;
     }
-
+    uint8_t* screenBuffer = new uint8_t[SCREEN_WIDTH * SCREEN_HEIGHT * 3];
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            screenBuffer[y * SCREEN_WIDTH * 3 + x * 3] = 0x25;
+            screenBuffer[y * SCREEN_WIDTH * 3 + x * 3 + 1] = 0x34;
+            screenBuffer[y * SCREEN_WIDTH * 3 + x * 3 + 2] = 0xff;
+        }
+    }
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawPixels(640, 480, GL_RGB, GL_UNSIGNED_BYTE, screenBuffer);
+        glfwSwapBuffers(window);
         glfwWaitEvents();
     }
 
